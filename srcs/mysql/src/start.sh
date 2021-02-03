2397be5dde2c49ab3c8927dbf3d7e82aaa0b358d
Adrian Munoz perez <amunoz-p@c2r8s2.42madrid.com>
2021-02-03 17:28:35 +0100

    metabl

--- srcs/mysql/src/start.sh
+++ srcs/mysql/src/start.sh
@@ -16,14 +16,19 @@ mariadb-install-db -u root
 # Invoking "mysqld" will start the MySQL server. Terminating "mysqld" will shutdown the MySQL server.
 mysqld -u root & sleep 5
 
+mysql << EOF 
+
 # Create Wordpress database.
 mysql -u root --execute="CREATE DATABASE wordpress;"
 
 # Import previously backed up database to MariaDB database server (wordpress < /wordpress.sql).
-mysql -u root wordpress < wordpress.sql
 
 # Create new user "root" with password "toor" and give permissions.
 mysql -u root --execute="CREATE USER 'root'@'%' IDENTIFIED BY 'toor'; GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION; USE wordpress; FLUSH PRIVILEGES;"
+EOF
 
+mysql -u root wordpress < wordpress.sql
 # Start Telegraf and sleep infinity for avoid container to stop.
 # telegraf & sleep infinite
+
+mysql -u root -p
